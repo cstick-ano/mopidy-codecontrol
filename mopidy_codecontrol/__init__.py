fbfa7ebaf6274837475f2f5b022810ffa7ac4970
cstick-ano <66933877+cstick-ano@users.noreply.github.com>
2021-07-07 18:22:37 +0700

    Update __init__.py

--- mopidy_codecontrol/__init__.py
+++ mopidy_codecontrol/__init__.py
@@ -1,1 +1,32 @@
-import logging
+    
+import os
+
+import tornado.web
+
+from mopidy import ext
+
+
+class MyRequestHandler(tornado.web.RequestHandler):
+    def initialize(self, core):
+        self.core = core
+
+    def get(self):
+        self.write(
+            'Hello, world! This is Mopidy %s' %
+            self.core.get_version().get())
+
+
+def my_app_factory(config, core):
+    return [
+        ('/', MyRequestHandler, {'core': core})
+    ]
+
+
+class MyWebClientExtension(ext.Extension):
+    ext_name = 'mywebclient'
+
+    def setup(self, registry):
+        registry.add('http:app', {
+            'name': self.ext_name,
+            'factory': my_app_factory,
+        })
