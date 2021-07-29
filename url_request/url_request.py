8e28d38e604e69fe451b5ff344fc0470d0ed6d9a
cstick-ano <66933877+cstick-ano@users.noreply.github.com>
2021-07-30 00:33:53 +0700

    Update url_request.py

--- url_request/url_request.py
+++ url_request/url_request.py
@@ -1,4 +1,5 @@
 import requests
+from tqdm import tqdm
 
 url = 'http://128.199.247.96:3000/api/music'
 playlist = requests.get(url,allow_redirects=True)
@@ -11,7 +12,7 @@ for i,enum in enumerate(playlist_lst):
     if enum == "', '":
         playlist_lst.pop(i)
 
-for j in range (0,len(playlist_lst)):
+for j in tqdm(range (0,len(playlist_lst))):
     music = playlist_lst[j].split('/')
     music_download = requests.get(playlist_lst[j],allow_redirects=True)
     open(music[-1],('wb')).write(music_download.content)
